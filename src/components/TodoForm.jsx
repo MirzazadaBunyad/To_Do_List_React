38ef705bb8fe4e7ce1dcedca9173a4ffed3a97b7
Mirzazada Bunyad <115315853+MirzazadaBunyad@users.noreply.github.com>
2024-02-14 15:19:03 +0400

    todolist

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -13,7 +13,7 @@ export default function TodoForm({ addTodo }) {
       <input
         type="text"
         className="todo-input"
-        placeholder="What are the tasks today?"
+        placeholder="What is the task today?"
         value={value}
         onChange={(e) => {
           setValue(e.target.value);
